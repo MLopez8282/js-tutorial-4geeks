f73ca1a87a30a6a476d62e5bc31491466d95ae41
MLopez8282 <saltairpix@gmail.com>
2020-11-25 12:43:00 +0000

    tutorial

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+const initGame = () => {
+    console.log("Init Game");
+    const images= [
+      "1.png",  "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png"
+    ]
+    let play = document.querySelector('#play');
+    let init = document.querySelector('.init');//we need to hide this element
+    let game = document.querySelector('.game');//we need to show this element
+    let cards = document.querySelector('.card');
+    let points = document.querySelector('.score-points');
 
+    play.addEventListener("click", () => {
+        init.style.display = "none";
+        game.style.display = "block";
+
+    });
+}
+
+initGame()
